73e88b5a88a622e9739516bcb3e2418245cfb4cf
vtammine <vtammine@ford.com>
2021-12-20 15:15:09 -0500

    Our very first flux

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,24 @@
 package com.learnreactiveprogramming.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import reactor.core.publisher.Flux;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+        List<String> list = new ArrayList<>();
+        list.add("alex");
+        list.add("ben");
+        list.add("chloe");
+       return Flux.fromIterable(list);
+    }
+
+    public static void main(String[] args) {
+       FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+       fluxAndMonoGeneratorService.namesFlux()
+               .subscribe(name-> {
+                   System.out.println("Name is :" + name);
+               });
+    }
 }
